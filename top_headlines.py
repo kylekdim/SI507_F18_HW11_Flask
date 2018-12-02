218fbcbb8b66092fb44f1ba649968e094350ff67
Kyle <kylegomi@gmail.com>
2018-12-02 18:09:17 -0500

    update top headlines, add index template

--- top_headlines.py
+++ top_headlines.py
@@ -6,6 +6,20 @@ import json
 
 app = Flask(__name__)
 
+@app.route('/user/<word>')
+def user_page(word):
+baseurl = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
+params_diction = {}
+params_diction["api-key"] = api_key # from secrets_example.py
+params_diction["section"] = 'technology'
+    
+nyt_resp = requests.get(baseurl, params_diction)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
